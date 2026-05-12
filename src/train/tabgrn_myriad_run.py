85d203a5227d2c666c1866b913dd6f5a257dc1d3
ChristianLangridge <chris.p.langridge@gmail.com>
2026-05-12 11:42:04 +0100

    More functional fixing

--- src/train/tabgrn_myriad_run.py
+++ src/train/tabgrn_myriad_run.py
@@ -38,6 +38,8 @@ import numpy as np
 import torch
 
 torch.backends.cuda.enable_flash_sdp(False)
+torch.backends.cuda.enable_mem_efficient_sdp(False)
+torch.backends.cuda.enable_math_sdp(True)
 
 from spatialmt.config.experiment import ExperimentConfig
 from spatialmt.config.paths import PROJECT_ROOT
